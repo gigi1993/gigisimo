2a692771fe0a355deaa601ec7b0724c44ed16c07
gigi1993 <gigi.tuttobene@gmail.com>
2015-05-02 11:57:02 +0200

    modified exercise 1

--- Exercise_01/Exercise_01/Program.cs
+++ Exercise_01/Exercise_01/Program.cs
@@ -4,26 +4,9 @@ namespace Exercise_01
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
-		{
-			int size, position;
-			Console.WriteLine ("Enter the size of the array");										// initialization of the array
-			size = int.Parse (Console.ReadLine ());
-			int[] numbers = new int[size];
-			for (int j = 0; j < size; j++){
-				Console.WriteLine ("Enter a number ({0}/{1})",j+1,size);
-				numbers[j] = int.Parse(Console.ReadLine());
-			}
-			Console.WriteLine ("Enter the position you want to check (from 0 to {0})",size-1);		// position to check
-			position = int.Parse(Console.ReadLine());
-			while (position < 0 || position > size-1) {
-				Console.WriteLine ("Enter the position you want to check (from 0 to {0})",size-1);	// check for a valid position
-				position = int.Parse(Console.ReadLine());
-			}
-			bool is_greater = GreaterThanNeighbours(numbers ,position);									// invoke the method
-			Console.WriteLine(is_greater);
-		}
-
+		
+		// cancellato il main
+		
 		static bool GreaterThanNeighbours(int[] array,int p)									// method
 		{
 			if (array.Length == 1) {																// if there is just one element
